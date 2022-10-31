ea5511d10009157cf6e454fcd8f26139405d80aa
Pedro Henrique de Carvalho Oliosi <111820294+PedroHenriqueOliosi@users.noreply.github.com>
2022-10-31 18:25:56 -0300

    Update JOGO.py

--- JOGO.py
+++ JOGO.py
@@ -21,47 +21,19 @@ class Jogo:
             self.desenha()
     
     def eventos(self):
-        pg.event.get()
-        
-        keys = pg.key.get_pressed()
-        if keys[pg.K_ESCAPE]:
-            sys.exit(0)
+       pressed = pg.key.get_pressed()
 
-        if keys[pg.K_w]:
-            self.player.mover_negativo()
-            self.player.atualizar_posy()
-        elif keys[pg.K_d]:
-            self.player.mover_positivo()
-            self.player.atualizar_posx()
-        elif keys[pg.K_s]:
-            self.player.mover_positivo()
-            self.player.atualizar_posy()
-        elif keys[pg.K_a]:
-            self.player.mover_negativo()
-            self.player.atualizar_posx() 
-        else:
-            self.player.parar()
+       dic = {
+           pg.K_w : self.player.mover_cima(),
+           pg.K_a : self.player.mover_esquerda(),
+           pg.K_s : self.player.mover_baixo(),
+           pg.K_d : self.player.mover_direita()
+       }
 
-        if keys[pg.K_w] and keys[pg.K_d]:   #x+ y-
-            self.player.mover_positivo_diagonal()
-            self.player.atualizar_posx()
-            self.player.mover_negativo_diagonal()
-            self.player.atualizar_posy()
-        elif keys[pg.K_w] and keys[pg.K_a]: #x- y-
-            self.player.mover_negativo_diagonal()
-            self.player.atualizar_posx()
-            self.player.atualizar_posy()
-        elif keys[pg.K_s] and keys[pg.K_d]: #x+ y+
-            self.player.mover_positivo_diagonal()
-            self.player.atualizar_posx()
-            self.player.atualizar_posy()
-        elif keys[pg.K_s] and keys[pg.K_a]: #x- y+
-            self.player.mover_negativo_diagonal()
-            self.player.atualizar_posx()
-            self.player.mover_positivo_diagonal()
-            self.player.atualizar_posy()
-        else:
-            self.player.parar()
+       for k, fn in dic.items():
+           if(pressed[k]) or (pg.event.type == pg.KEYDOWN and pg.event.key == k):
+              fn()
+
     def desenha(self):
         self.screen.fill('white')
         self.player.desenho(self.screen)
