93c5dab33ff2b85f97ef6ddebd0e44cf368aa20b
pcatrinck <108846665+pcatrinck@users.noreply.github.com>
2022-11-28 23:45:54 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ class Game:
         self.screen = pg.display.set_mode((LARGURA, ALTURA))
         self.clock = pg.time.Clock()
         self.running = True
+        self.font = pg.font.Font("fonte_jogo.TTF", 34)
+        self.little_font = pg.font.Font("fontinha_jogo.TTF", 14)
 
         self.character_spritesheet = Spritesheet('C:\POO\TRAB_1\character.png')
         self.terrain_spritesheet = Spritesheet('C:\POO\TRAB_1\Terrain.png')
         self.enemy_spritesheet = Spritesheet('C:\POO\TRAB_1\enemy.png')
+        self.attack_spritesheet = Spritesheet('C:\POO\TRAB_1\Attack.png')
+        self.intro_background = pg.image.load('C:\POO\TRAB_1\introbackground.png')
 
     def createTilemap(self):
         for i, row in enumerate(tilemap):
@@ -23,7 +27,7 @@ class Game:
                 if column == "E":
                     Enemy(self, j, i)
                 if column == "P":
-                    Player(self, j, i)
+                    self.player = Player(self, j, i)
 
     def new(self):
         #a new game starts
@@ -43,6 +47,18 @@ class Game:
             if event.type == pg.QUIT or keys[pg.K_ESCAPE]:
                 self.playing = False
                 self.running = False
+                
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    if self.player.facing == 'up':
+                        Attack(self, self.player.rect.x, self.player.rect.y - TILESIZE)
+                    if self.player.facing == 'down':
+                        Attack(self, self.player.rect.x, self.player.rect.y + TILESIZE)
+                    if self.player.facing == 'left':
+                        Attack(self, self.player.rect.x - TILESIZE, self.player.rect.y)
+                    if self.player.facing == 'right':
+                        Attack(self, self.player.rect.x + TILESIZE, self.player.rect.y)
+                    
     
     def update (self):
         self.all_sprites.update()
@@ -64,11 +80,67 @@ class Game:
     def game_over(self):
         pass
 
-    def intro_screen(self):
-        pass
+    def intro_screen(self, intro):
+        self.intro = intro
+
+        title = self.font.render('Joguissimo', True, BLACK)
+        title_rect = title.get_rect(x=10, y=10)
+
+        play_button = Button(10, 76, 100, 50, WHITE, BLACK, 'Play', 18)
+        lore_button = Button(10, 140, 100, 50, WHITE, BLACK, 'Lore', 18)
+
+        while self.intro:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.intro = False
+                    self.running = False
+            
+            mouse_pos = pg.mouse.get_pos()
+            mouse_pressed = pg.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                self.intro = False
+            if lore_button.is_pressed(mouse_pos, mouse_pressed):
+                self.intro = False
+                g.lore_screen()
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.screen.blit(lore_button.image, lore_button.rect)
+
+            self.clock.tick(FPS)
+            pg.display.update()
+
+    def lore_screen (self):
+        lore = True
+
+        title = self.font.render('Lore', True, BLACK)
+        title_rect = title.get_rect(x=10, y=10)
+
+        intro_button = Button(10, 76, 100, 50, WHITE, BLACK, 'Back', 18)
+
+        while lore:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+            
+            mouse_pos = pg.mouse.get_pos()
+            mouse_pressed = pg.mouse.get_pressed()
+
+            if intro_button.is_pressed(mouse_pos, mouse_pressed):
+                lore = False
+                g.intro_screen(True)
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(intro_button.image, intro_button.rect)
+
+            self.clock.tick(FPS)
+            pg.display.update()
 
 g = Game()
-g.intro_screen()
+g.intro_screen(True)
 g.new()
 while g.running:
     g.main()
